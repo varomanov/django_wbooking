b77d1cfc009492db4207c93e0d9a4a3382c6e924
Vladimir Romanov <vladimirromanov@me.com>
2025-12-28 20:02:17 +0300

    init

--- SERVIER/urls.py
+++ SERVIER/urls.py
@@ -5,7 +5,7 @@ from WorkplaceBooking import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.booking, name='booking'),
-    path('/journal', views.journal, name='journal'),
-    path('/floor', views.floor, name='floor'),
-    path('/kir', views.kir, name='kir'),
+    path('journal', views.journal, name='journal'),
+    path('floor', views.floor, name='floor'),
+    path('kir', views.kir, name='kir'),
 ]
